80163189163f60c35fcd47c90093ea51498d47f7
Andolo2 <andreasolofsson123@gmail.com>
2022-12-07 21:00:41 +0100

    detailsView is working

--- controllers/ProductsController_NEW.js
+++ controllers/ProductsController_NEW.js
@@ -4,39 +4,96 @@ const controller = express.Router()
 const productSchema = require('../schemas/productSchema_EXPRESS')
 
 
-/********************************************************************************************************************/ 
+/****Unsecured Routes********Unsecured Routes********Unsecured Routes********Unsecured Routes********Unsecured Routes********Unsecured Routes****/ 
 
 // Unsecured Routes
 controller.route('/').get(async (httpRequest, httpResponse) =>  {
 
-   try{
-          const product = await productSchema.find()
-          httpResponse.status(200).json(product)
-                //{ success: true, message: 'All products has been added ', data: product }
-        } catch{
-        httpResponse.status(400).json({ success: false, message: 'No products has been added ', data: product })
-        }
+        const products = []
+
+        const productList = await productSchema.find()
+        if(productList){
+                
+                for( let product of productList){
+                        products.push({
+                                articleNumber: product._id,
+                                name: product.name,
+                                description: product.description,
+                                price: product.price,
+                                category: product.category,
+                                tag: product.tag,
+                                imageName: product.imageName,
+                                rating: product.rating
+
+                        })
+                }
+
+                httpResponse.status(200).json(products)
+                console.log(products)
+        } 
+        else
+        httpResponse.status(400).json()
 
 })
 
 /********************************************************************************************************************/ 
 
 controller.route('/:tag').get(async(httpRequest, httpResponse, tag) =>  { // product based on tag
-       const products = await productSchema.find({tag: httpRequest.params.tag})
-       if(products){
-        httpResponse.status(200).json(products)
-       } else
-       httpResponse.status(400).json()
+
+        const products = []
+
+        const productList = await productSchema.find({tag: httpRequest.params.tag})
+        if(productList){
+                
+                for( let product of productList){
+                        products.push({
+                                articleNumber: product._id,
+                                name: product.name,
+                                description: product.description,
+                                price: product.price,
+                                category: product.category,
+                                tag: product.tag,
+                                imageName: product.imageName,
+                                rating: product.rating
+
+                        })
+                }
+
+                httpResponse.status(200).json(products)
+                
+        } 
+        else
+        httpResponse.status(400).json()
 
 })     
 
 /********************************************************************************************************************/ 
 
 controller.route('/:tag/:take').get(async(httpRequest, httpResponse) =>  { // ex url: http://localhost:5000/api/products/featured/1
-        const products = await productSchema.find({tag: httpRequest.params.tag}).limit(httpRequest.params.take)
-        if(products){
+        
+        const products = []
+        
+        const productList = await productSchema.find({tag: httpRequest.params.tag}).limit(httpRequest.params.take)
+        if(productList){
+                
+                for( let product of productList){
+                        products.push({
+                                articleNumber: product._id,
+                                name: product.name,
+                                description: product.description,
+                                price: product.price,
+                                category: product.category,
+                                tag: product.tag,
+                                imageName: product.imageName,
+                                rating: product.rating
+
+                        })
+                }
+
                 httpResponse.status(200).json(products)
-        } else
+
+        } 
+        else
         httpResponse.status(400).json()
 
 })
@@ -47,7 +104,25 @@ controller.route('/product/details/:articleNumber').get(async(httpRequest, httpR
 
         const product = await productSchema.findById(httpRequest.params.articleNumber)
         if(product){
-         httpResponse.status(200).json(product)
+                
+                if(product){
+                 
+                httpResponse.status(200).json({
+
+                        articleNumber: product._id,
+                        name: product.name,
+                        description: product.description,
+                        price: product.price,
+                        category: product.category,
+                        tag: product.tag,
+                        imageName: product.imageName,
+                        rating: product.rating
+
+                        })
+                } 
+                else
+                httpResponse.status(400).json()
+        
         } else
         httpResponse.status(404).json()
 })
@@ -55,7 +130,13 @@ controller.route('/product/details/:articleNumber').get(async(httpRequest, httpR
 /********************************************************************************************************************/ 
 
 
-// Secure routes
+/*******Secured Routes***************Secured Routes***************Secured Routes***************Secured Routes***************Secured Routes********/
+
+
+
+
+
+
 
 
 module.exports = controller
